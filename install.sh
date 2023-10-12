8a39e8aa47dab0c4c2a03df7bfd4a25002168849
James Goenawan <james12345678922@gmail.com>
2023-10-12 14:14:14 +0800

    update

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ sudo id -u
 
 msg "Installing Dependencies 🏗️"
 sudo apt update
-sudo apt install git tmux tmuxp curl htop nvtop build-essential net-tools snap -y
+sudo apt install git tmux tmuxp curl htop nvtop build-essential net-tools snap libxdo3 libva-drm2 libva-xll-2 libvdpaul gstreamer1.0-pipewire libnss3-tools gnupg2 nftables  -y
 
 msg "Installing Spotify 🎶"
 sudo snap install spotify
@@ -41,7 +41,8 @@ sleep 10
 msg "Installing Cloudflare-Warp VPN 📶"
 curl https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
-sudo apt-get update && sudo apt-get install cloudflare-warp
+sudo apt update 
+sudo apt install cloudflare-warp -y
 
 
 # miniconda 
